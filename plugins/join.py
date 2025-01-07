00440540ee5638c10e039e8454bd59101e7db11a
SURAJSALAINI12 <surajrajsaini218@gmail.com>
2025-01-07 10:21:21 +0530

    Update join.py

--- plugins/join.py
+++ plugins/join.py
@@ -5,7 +5,7 @@ from BADMUSIC import app
 
 #--------------------------
 
-MUST_JOIN = "PBX_CHAT"
+MUST_JOIN = "quizbysu"
 #------------------------
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
@@ -26,7 +26,7 @@ async def must_join_channel(app: Client, msg: Message):
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("๏Jᴏɪɴ¹๏", url=f"https://t.me/HEROKUBIN_01"),
+                                InlineKeyboardButton("๏Jᴏɪɴ¹๏", url=f"https://t.me/quizbys"),
                             ],
                             [
                             InlineKeyboardButton("๏Jᴏɪɴ²๏", url=link),
